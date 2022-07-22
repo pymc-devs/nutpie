1342b07143b4a2c83f5a3508e972729dcb8deb55
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2022-07-21 20:52:04 -0500

    Fix typo in error message

--- nutpie/__init__.py
+++ nutpie/__init__.py
@@ -4,12 +4,12 @@ try:
     from .compile_pymc import compile_pymc_model
 except ImportError:
     def compile_pymc_model(*args, **kwargs):
-        raise ValueError("Missing dependencies for pymc. `import nutpy.compile_pymc` to see error.")
+        raise ValueError("Missing dependencies for pymc. `import nutpie.compile_pymc` to see error.")
 
 try:
     from .compile_stan import compile_stan_model
 except ImportError:
     def compile_stan_model(*args, **kwargs):
-        raise ImportError("Missing dependencies for stan. `import nutpy.comile_stan` to see error.")
+        raise ImportError("Missing dependencies for stan. `import nutpie.compile_stan` to see error.")
 
 from .sample import sample
