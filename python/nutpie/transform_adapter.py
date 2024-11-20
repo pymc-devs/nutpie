bbed6386921365c56bc5f4a295fde3c7bb5fc20f
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2024-11-20 16:29:25 +0100

    Work on default normalizing flow

--- python/nutpie/transform_adapter.py
+++ python/nutpie/transform_adapter.py
@@ -8,10 +8,14 @@ def make_transform_adapter(
     num_layers=8,
     num_diag_windows=10,
     learning_rate=1e-3,
-    scale_layer=False,
     untransformed_dim=None,
     zero_init=True,
     batch_size=128,
+    reuse_opt_state=True,
+    max_patience=5,
+    householder_layer=True,
+    dct_layer=True,
+    gamma=None,
 ):
     import traceback
     from functools import partial
@@ -27,6 +31,9 @@ def make_transform_adapter(
     from paramax import Parameterize, unwrap
 
     class FisherLoss:
+        def __init__(self, gamma=None):
+            self._gamma = gamma
+
         @eqx.filter_jit
         def __call__(
             self,
@@ -40,10 +47,15 @@ def make_transform_adapter(
             flow = unwrap(eqx.combine(params, static, is_leaf=eqx.is_inexact_array))
 
             def compute_loss(bijection, draw, grad):
-                draw, grad, _ = bijection.inverse_gradient_and_val(
+                draw, grad, logp = bijection.inverse_gradient_and_val(
                     draw, grad, jnp.array(0.0)
                 )
-                return ((draw + grad) ** 2).sum()
+                cost = ((draw + grad) ** 2).sum()
+                if self._gamma is not None:
+                    normal_logp = -draw @ draw / 2
+                    cost = cost + self._gamma * (logp - normal_logp).sum()
+
+                return cost
 
             assert x.shape[1] == 2
             draws = x[:, 0, :]
@@ -72,9 +84,10 @@ def make_transform_adapter(
             dist=flow,
             x=points,
             loss_fn=loss_fn,
+            max_epochs=500,
             **kwargs,
         )
-        return fit.bijection, losses
+        return fit.bijection, losses, losses["opt_state"]
 
     def make_flow(
         seed,
@@ -82,12 +95,16 @@ def make_transform_adapter(
         gradients,
         *,
         zero_init=False,
-        scale_layer=False,
-        untransformed_dim=None,
+        householder_layer=False,
+        dct_layer=False,
+        untransformed_dim: int | list[int] | None = None,
         n_layers,
     ):
         positions = np.array(positions)
         gradients = np.array(gradients)
+
+        if len(positions) == 0:
+            return
 
         n_draws, n_dim = positions.shape
 
@@ -95,7 +112,6 @@ def make_transform_adapter(
             n_layers = 0
 
         assert positions.shape == gradients.shape
-        assert n_draws > 0
 
         if n_draws == 0:
             raise ValueError("No draws")
@@ -119,7 +135,12 @@ def make_transform_adapter(
         if n_layers == 0:
             return flowjax.flows.Chain(flows)
 
-        def make_layer(key, is_last=False):
+        if untransformed_dim is None:
+            untransformed_dim = n_dim // 2
+
+        untransformed_dim = cast(list[int] | int, untransformed_dim)
+
+        def make_layer(key, untransformed_dim, is_last=False):
             key, key_couple, key_permute = jax.random.split(key, 3)
 
             scale = Parameterize(
@@ -138,15 +159,10 @@ def make_transform_adapter(
             else:
                 width = nn_width
 
-            if untransformed_dim is None:
-                untrans_dim = n_dim // 2
-            else:
-                untrans_dim = untransformed_dim
-
             coupling = flowjax.bijections.coupling.Coupling(
                 key_couple,
                 transformer=affine,
-                untransformed_dim=untrans_dim,
+                untransformed_dim=untransformed_dim,
                 dim=n_dim,
                 nn_activation=jax.nn.gelu,
                 nn_width=width,
@@ -163,27 +179,67 @@ def make_transform_adapter(
             else:
                 flow = flowjax.flows._add_default_permute(coupling, n_dim, key_permute)
 
-            if scale_layer:
-                from flowjax.bijections import mvscale
-
+            if householder_layer:
                 bijections = list(flow.bijections)
-                bijections.append(mvscale.MvScale4(jnp.ones(n_dim) * 1e-5))
-                # bijections.append(mvscale.MvScale3(jnp.ones(n_dim) * 1e-5))
+                params = jnp.ones(n_dim) * 1e-5
+                params = params.at[0].set(0.)
+                bijections.append(flowjax.bijections.Householder(params))
                 flow = flowjax.bijections.Chain(bijections)
 
             return flow
 
         if n_layers == 1:
-            bijection = make_layer(key, is_last=True)
+            num_untrafo = (
+                untransformed_dim
+                if isinstance(untransformed_dim, int)
+                else untransformed_dim[-1]
+            )
+            bijection = make_layer(
+                key,
+                untransformed_dim=num_untrafo,
+                is_last=True,
+            )
         else:
             keys = jax.random.split(key, n_layers - 1)
-            layers = eqx.filter_vmap(make_layer)(keys)
-            bijection = flowjax.bijections.Scan(layers)
 
-            key, key_layer = jax.random.split(key)
-            last = make_layer(key_layer, is_last=True)
+            if isinstance(untransformed_dim, int):
+                make_layers = eqx.filter_vmap(make_layer)
+                layers = make_layers(keys, untransformed_dim)
+                bijection = flowjax.bijections.Scan(layers)
 
-            bijection = flowjax.bijections.Chain([bijection, last])
+                key, key_layer = jax.random.split(key)
+                last = make_layer(key_layer, untransformed_dim, is_last=True)
+
+                bijection = flowjax.bijections.Chain([bijection, last])
+            else:
+                layers = []
+                for i, (key, num_untrafo) in enumerate(zip(keys[:-1], untransformed_dim[:1])):
+
+                    if i % 2 == 0 or not dct_layer:
+                        layers.append(make_layer(key, num_untrafo))
+                    else:
+                        inner = make_layer(key, num_untrafo)
+                        outer = flowjax.bijections.DCT(inner.shape)
+
+                        layers.append(flowjax.bijections.Sandwich(outer, inner))
+
+                        scale_val = jnp.ones(n_dim)
+                        scale = Parameterize(
+                            lambda x: x + jnp.sqrt(1 + x**2),
+                            jnp.zeros(n_dim),
+                        )
+                        mean = jnp.zeros(n_dim)
+                        inner = eqx.tree_at(
+                            where=lambda aff: aff.scale,
+                            pytree=flowjax.bijections.Affine(mean, scale_val),
+                            replace=scale,
+                        )
+                        outer = flowjax.bijections.DCT(inner.shape)
+                        layers.append(flowjax.bijections.Sandwich(outer, inner))
+
+                layers.append(make_layer(keys[-1], untransformed_dim[-1], is_last=True))
+
+                bijection = flowjax.bijections.Chain(layers)
 
         return flowjax.bijections.Chain([bijection, *flows])
 
@@ -273,6 +329,9 @@ def make_transform_adapter(
             zero_init=True,
             untransformed_dim=None,
             batch_size=128,
+            reuse_opt_state=True,
+            max_patience=5,
+            gamma=None,
         ):
             self._logp_fn = logp_fn
             self._make_flow_fn = make_flow_fn
@@ -280,12 +339,16 @@ def make_transform_adapter(
             self._verbose = verbose
             self._window_size = window_size
             self._optimizer = optax.apply_if_finite(optax.adabelief(learning_rate), 50)
-            self._loss_fn = FisherLoss()
+            self._loss_fn = FisherLoss(gamma)
             self._show_progress = show_progress
             self._num_diag_windows = num_diag_windows
             self._zero_init = zero_init
             self._untransformed_dim = untransformed_dim
             self._batch_size = batch_size
+            self._reuse_opt_state = reuse_opt_state
+            self._opt_state = None
+            self._max_patience = max_patience
+            self._count_trace = []
             try:
                 self._bijection = make_flow_fn(seed, [position], [gradient], n_layers=0)
             except Exception as e:
@@ -303,13 +366,19 @@ def make_transform_adapter(
             if self._verbose:
                 print(f"Chain {self._chain}: Total available points: {len(positions)}")
             n_draws = len(positions)
+            self._count_trace.append(n_draws)
             if n_draws == 0:
                 return
             try:
                 if self.index <= self._num_diag_windows:
                     size = len(positions)
-                    positions = positions[-size // 5 + 3 :]
-                    gradients = gradients[-size // 5 + 3 :]
+                    lower_idx = -size // 5 + 3
+                    positions_slice = positions[-size // 5 + 3 :]
+                    gradients_slice = gradients[-size // 5 + 3 :]
+
+                    if len(positions_slice) > 0:
+                        positions = positions_slice
+                        gradients = gradients_slice
 
                     fit = self._make_flow_fn(seed, positions, gradients, n_layers=0)
                     points = jnp.transpose(jnp.array([positions, gradients]), [1, 0, 2])
@@ -320,10 +389,12 @@ def make_transform_adapter(
                     params, static = eqx.partition(flow, eqx.is_inexact_array)
                     new_loss = self._loss_fn(params, static, points)
 
-                    print("loss from diag:", new_loss)
+                    if self._verbose:
+                        print("loss from diag:", new_loss)
 
                     if np.isfinite(new_loss):
                         self._bijection = fit
+                        self._opt_state = None
 
                     return
 
@@ -369,9 +440,10 @@ def make_transform_adapter(
                 # make_flow might still only return a single trafo for 1d problems
                 if len(base.bijections) == 1:
                     self._bijection = base
+                    self._opt_state = None
                     return
 
-                fit, _ = fit_flow(
+                fit, _, opt_state = fit_flow(
                     key,
                     base,
                     self._loss_fn,
@@ -379,6 +451,8 @@ def make_transform_adapter(
                     show_progress=self._show_progress,
                     optimizer=self._optimizer,
                     batch_size=self._batch_size,
+                    opt_state=self._opt_state if self._reuse_opt_state else None,
+                    max_patience=self._max_patience,
                 )
 
                 flow = flowjax.flows.Transformed(
@@ -426,6 +500,7 @@ def make_transform_adapter(
                     self._bijection = self._make_flow_fn(
                         seed, positions, gradients, n_layers=0
                     )
+                    self._opt_state = None
                     return
 
                 if not np.isfinite(new_loss):
@@ -435,6 +510,7 @@ def make_transform_adapter(
                     return
 
                 self._bijection = fit
+                self._opt_state = opt_state
 
             except Exception as e:
                 print("update error:", e)
@@ -523,11 +599,18 @@ def make_transform_adapter(
         TransformAdapter,
         verbose=verbose,
         window_size=window_size,
-        make_flow_fn=partial(make_flow, scale_layer=scale_layer),
+        make_flow_fn=partial(
+            make_flow,
+            householder_layer=householder_layer,
+            dct_layer=dct_layer,
+        ),
         show_progress=show_progress,
         num_diag_windows=num_diag_windows,
         learning_rate=learning_rate,
         zero_init=zero_init,
         untransformed_dim=untransformed_dim,
         batch_size=batch_size,
+        reuse_opt_state=reuse_opt_state,
+        max_patience=max_patience,
+        gamma=gamma,
     )
