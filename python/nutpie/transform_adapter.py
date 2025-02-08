d68ee680ace62b9bad123a3e4a060343681c443e
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2025-02-08 22:51:37 +0100

    Allow length zero in normalizing flow layer

--- python/nutpie/transform_adapter.py
+++ python/nutpie/transform_adapter.py
@@ -4,9 +4,102 @@ import math
 import numpy as np
 import equinox as eqx
 import jax
+import jax.numpy as jnp
 
 _BIJECTION_TRACE = []
 
+class Linear(eqx.Module, strict=True):
+    """Performs a linear transformation."""
+
+    weight: jax.Array
+    bias: jax.Array | None
+    in_features: Union[int, Literal["scalar"]] = eqx.field(static=True)
+    out_features: Union[int, Literal["scalar"]] = eqx.field(static=True)
+    use_bias: bool = eqx.field(static=True)
+
+    def __init__(
+        self,
+        in_features: Union[int, Literal["scalar"]],
+        out_features: Union[int, Literal["scalar"]],
+        use_bias: bool = True,
+        dtype=None,
+        *,
+        key,
+    ):
+        """**Arguments:**
+
+        - `in_features`: The input size. The input to the layer should be a vector of
+            shape `(in_features,)`
+        - `out_features`: The output size. The output from the layer will be a vector
+            of shape `(out_features,)`.
+        - `use_bias`: Whether to add on a bias as well.
+        - `dtype`: The dtype to use for the weight and the bias in this layer.
+            Defaults to either `jax.numpy.float32` or `jax.numpy.float64` depending
+            on whether JAX is in 64-bit mode.
+        - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+
+        Note that `in_features` also supports the string `"scalar"` as a special value.
+        In this case the input to the layer should be of shape `()`.
+
+        Likewise `out_features` can also be a string `"scalar"`, in which case the
+        output from the layer will have shape `()`.
+        """
+        #dtype = default_floating_dtype() if dtype is None else dtype
+        dtype = np.float32 if dtype is None else dtype
+        wkey, bkey = jax.random.split(key, 2)
+        in_features_ = 1 if in_features == "scalar" else in_features
+        out_features_ = 1 if out_features == "scalar" else out_features
+        if in_features_ == 0:
+            lim = 1.0
+        else:
+            lim = 1 / math.sqrt(in_features_)
+        wshape = (out_features_, in_features_)
+        self.weight = eqx.nn._misc.default_init(wkey, wshape, dtype, lim)
+        bshape = (out_features_,)
+        self.bias = eqx.nn._misc.default_init(bkey, bshape, dtype, lim) if use_bias else None
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.use_bias = use_bias
+
+    @jax.named_scope("eqx.nn.Linear")
+    def __call__(self, x: jax.Array, *, key=None) -> jax.Array:
+        """**Arguments:**
+
+        - `x`: The input. Should be a JAX array of shape `(in_features,)`. (Or shape
+            `()` if `in_features="scalar"`.)
+        - `key`: Ignored; provided for compatibility with the rest of the Equinox API.
+            (Keyword only argument.)
+
+        !!! info
+
+            If you want to use higher order tensors as inputs (for example featuring "
+            "batch dimensions) then use `jax.vmap`. For example, for an input `x` of "
+            "shape `(batch, in_features)`, using
+            ```python
+            linear = equinox.nn.Linear(...)
+            jax.vmap(linear)(x)
+            ```
+            will produce the appropriate output of shape `(batch, out_features)`.
+
+        **Returns:**
+
+        A JAX array of shape `(out_features,)`. (Or shape `()` if
+        `out_features="scalar"`.)
+        """
+
+        if self.in_features == "scalar":
+            if jnp.shape(x) != ():
+                raise ValueError("x must have scalar shape")
+            x = jnp.broadcast_to(x, (1,))
+        x = self.weight @ x
+        if self.bias is not None:
+            x = x + self.bias
+        if self.out_features == "scalar":
+            assert jnp.shape(x) == (1,)
+            x = jnp.squeeze(x)
+        return x
 
 class FactoredMLP(eqx.Module, strict=True):
     """Standard Multi-Layer Perceptron; also known as a feed-forward network.
@@ -16,7 +109,7 @@ class FactoredMLP(eqx.Module, strict=True):
         If you get a TypeError saying an object is not a valid JAX type, see the
             [FAQ](https://docs.kidger.site/equinox/faq/)."""
 
-    layers: tuple[tuple[eqx.nn.Linear, eqx.nn.Linear], ...]
+    layers: tuple[tuple[Linear, Linear], ...]
     activation: tuple[Callable, ...]
     final_activation: Callable
     use_bias: bool = eqx.field(static=True)
@@ -82,19 +175,19 @@ class FactoredMLP(eqx.Module, strict=True):
 
         if depth == 0:
             layers.append(
-                eqx.nn.Linear(in_size, out_size, use_final_bias, dtype=dtype, key=keys[0])
+                Linear(in_size, out_size, use_final_bias, dtype=dtype, key=keys[0])
             )
         else:
             if isinstance(width_size[0], tuple):
                 n, k = width_size[0]
                 key1, key2 = jax.random.split(keys[0])
-                U = eqx.nn.Linear(in_size, n, use_bias=False, dtype=dtype, key=key1)
-                K = eqx.nn.Linear(n, k, use_bias=True, dtype=dtype, key=key2)
+                U = Linear(in_size, n, use_bias=False, dtype=dtype, key=key1)
+                K = Linear(n, k, use_bias=True, dtype=dtype, key=key2)
                 layers.append((U, K))
             else:
                 k = width_size[0]
                 layers.append(
-                    eqx.nn.Linear(in_size, k, use_bias, dtype=dtype, key=keys[0])
+                    Linear(in_size, k, use_bias, dtype=dtype, key=keys[0])
                 )
             activations.append(eqx.filter_vmap(lambda: activation, axis_size=k)())
 
@@ -102,13 +195,13 @@ class FactoredMLP(eqx.Module, strict=True):
                 if isinstance(width_size[i + 1], tuple):
                     n, k_new = width_size[i + 1]
                     key1, key2 = jax.random.split(keys[i + 1])
-                    U = eqx.nn.Linear(k, n, use_bias=False, dtype=dtype, key=key1)
-                    K = eqx.nn.Linear(n, k_new, use_bias=True, dtype=dtype, key=key2)
+                    U = Linear(k, n, use_bias=False, dtype=dtype, key=key1)
+                    K = Linear(n, k_new, use_bias=True, dtype=dtype, key=key2)
                     layers.append((U, K))
                     k = k_new
                 else:
                     layers.append(
-                        eqx.nn.Linear(
+                        Linear(
                             k, width_size[i + 1], use_bias, dtype=dtype, key=keys[i + 1]
                         )
                     )
@@ -118,13 +211,13 @@ class FactoredMLP(eqx.Module, strict=True):
             if isinstance(out_size, tuple):
                 n, k_new = out_size
                 key1, key2 = jax.random.split(keys[-1])
-                U = eqx.nn.Linear(k, n, use_bias=False, dtype=dtype, key=key1)
-                K = eqx.nn.Linear(n, k_new, use_bias=True, dtype=dtype, key=key2)
+                U = Linear(k, n, use_bias=False, dtype=dtype, key=key1)
+                K = Linear(n, k_new, use_bias=True, dtype=dtype, key=key2)
                 k = k_new
                 layers.append((U, K))
             else:
                 layers.append(
-                    eqx.nn.Linear(k, out_size, use_final_bias, dtype=dtype, key=keys[-1])
+                    Linear(k, out_size, use_final_bias, dtype=dtype, key=keys[-1])
                 )
         self.layers = tuple(layers)
         self.in_size = in_size
@@ -193,7 +286,7 @@ class MLP(eqx.Module, strict=True):
         If you get a TypeError saying an object is not a valid JAX type, see the
             [FAQ](https://docs.kidger.site/equinox/faq/)."""
 
-    layers: tuple[eqx.nn.Linear, ...]
+    layers: tuple[Linear, ...]
     activation: tuple[Callable, ...]
     final_activation: Callable
     use_bias: bool = eqx.field(static=True)
@@ -259,16 +352,16 @@ class MLP(eqx.Module, strict=True):
 
         if depth == 0:
             layers.append(
-                eqx.nn.Linear(in_size, out_size, use_final_bias, dtype=dtype, key=keys[0])
+                Linear(in_size, out_size, use_final_bias, dtype=dtype, key=keys[0])
             )
         else:
             layers.append(
-                eqx.nn.Linear(in_size, width_size[0], use_bias, dtype=dtype, key=keys[0])
+                Linear(in_size, width_size[0], use_bias, dtype=dtype, key=keys[0])
             )
             activations.append(eqx.filter_vmap(lambda: activation, axis_size=width_size[0])())
             for i in range(depth - 1):
                 layers.append(
-                    eqx.nn.Linear(
+                    Linear(
                         width_size[i], width_size[i + 1], use_bias, dtype=dtype, key=keys[i + 1]
                     )
                 )
