d830c10a3882bf993df83023602c57eefbe7deff
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2025-05-06 21:30:17 +0200

    fix: correctly handle tuples in stan traces

--- tests/test_stan.py
+++ tests/test_stan.py
@@ -65,6 +65,90 @@ def test_seed():
 
 
 @pytest.mark.stan
+def test_nested():
+    # Adapted from
+    # https://github.com/stan-dev/stanio/blob/main/test/data/tuples/output.stan
+    model = """
+    parameters {
+    real a;
+    }
+    model {
+    a ~ normal(0, 1);
+    }
+    generated quantities {
+    real base = normal_rng(0, 1);
+    int base_i = to_int(normal_rng(10, 10));
+
+    tuple(real, real) pair = (base, base * 2);
+
+    tuple(real, tuple(int, complex)) nested = (base * 3, (base_i, base * 4.0i));
+    array[2] tuple(real, real) arr_pair = {pair, (base * 5, base * 6)};
+
+    array[3] tuple(tuple(real, tuple(int, complex)), real) arr_very_nested
+        = {(nested, base*7), ((base*8, (base_i*2, base*9.0i)), base * 10), (nested, base*11)};
+
+    array[3,2] tuple(real, real) arr_2d_pair = {{(base * 12, base * 13), (base * 14, base * 15)},
+                                                {(base * 16, base * 17), (base * 18, base * 19)},
+                                                {(base * 20, base * 21), (base * 22, base * 23)}};
+
+    real basep1 = base + 1, basep2 = base + 2;
+    real basep3 = base + 3, basep4 = base + 4, basep5 = base + 5;
+    array[2,3] tuple(array[2] tuple(real, vector[2]), matrix[4,5]) ultimate =
+        {
+        {(
+            {(base, [base *2, base *3]'), (base *4, [base*5, base*6]')},
+            to_matrix(linspaced_vector(20, 7, 11), 4, 5) * base
+            ),
+        (
+            {(basep1, [basep1 *2, basep1 *3]'), (basep1 *4, [basep1*5, basep1*6]')},
+            to_matrix(linspaced_vector(20, 7, 11), 4, 5) * basep1
+            ),
+            (
+            {(basep2, [basep2 *2, basep2 *3]'), (basep2 *4, [basep2*5, basep2*6]')},
+            to_matrix(linspaced_vector(20, 7, 11), 4, 5) * basep2
+        )
+        },
+        {(
+            {(basep3, [basep3 *2, basep3 *3]'), (basep3 *4, [basep3*5, basep3*6]')},
+            to_matrix(linspaced_vector(20, 7, 11), 4, 5) * basep3
+            ),
+        (
+            {(basep4, [basep4 *2, basep4 *3]'), (basep4 *4, [basep4*5, basep4*6]')},
+            to_matrix(linspaced_vector(20, 7, 11), 4, 5) * basep4
+            ),
+            (
+            {(basep5, [basep5 *2, basep5 *3]'), (basep5 *4, [basep5*5, basep5*6]')},
+            to_matrix(linspaced_vector(20, 7, 11), 4, 5) * basep5
+        )
+        }};
+    }
+    """
+
+    compiled = nutpie.compile_stan_model(code=model)
+    tr = nutpie.sample(compiled, chains=6)
+    base = tr.posterior.base
+
+    assert np.allclose(tr.posterior["nested:2:2.imag"], 4 * base)
+    assert np.allclose(tr.posterior["nested:2:2.real"], 0.0)
+
+    assert np.allclose(tr.posterior["ultimate.1.1:1.1:1"], base)
+    assert np.allclose(tr.posterior["ultimate.1.2:1.1:1"], base + 1)
+    assert np.allclose(tr.posterior["ultimate.1.3:1.1:1"], base + 2)
+    assert np.allclose(tr.posterior["ultimate.2.1:1.1:1"], base + 3)
+    assert np.allclose(tr.posterior["ultimate.2.2:1.1:1"], base + 4)
+    assert np.allclose(tr.posterior["ultimate.2.3:1.1:1"], base + 5)
+
+    assert tr.posterior["ultimate.2.1:1.1:2"].shape == (6, 1000, 2)
+    assert np.allclose(
+        tr.posterior["ultimate.2.3:1.1:2"].values[:, :, 0], 2 * (base + 5)
+    )
+    assert np.allclose(
+        tr.posterior["ultimate.2.3:1.1:2"].values[:, :, 1], 3 * (base + 5)
+    )
+    assert np.allclose(tr.posterior["base_i"], tr.posterior.base_i.astype(int))
+
+
+@pytest.mark.stan
 def test_stan_model_data():
     model = """
     data {
